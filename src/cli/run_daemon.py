9069254a9ecf75e0053cbdc54359c3a5c8cc4115
Loann Peurey <loannpeurey@gmail.com>
2025-08-11 15:46:20 +0200

    feat: add bootstrap and launch of main service loop to the cli call

--- src/cli/run_daemon.py
+++ src/cli/run_daemon.py
@@ -1,9 +1,19 @@
+from src.bootstrap.bootstrap import bootstrap
+
+from pathlib import Path
 import click
 
 
 @click.command()
-def run_daemon() -> None:
-    raise NotImplementedError
+@click.option(
+    "--config", "-c",
+    type=click.Path(exists=True, dir_okay=False, path_type=str),
+    default='configuration.toml',
+    help="Path to a TOML config file (overrides default)."
+)
+def run_daemon(config) -> None:
+    service = bootstrap(Path(config))
+    service.main_loop()
 
 
 # For the purpose of debugging
