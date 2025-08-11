e335dccdbd909538ccb0f4c09d3bceaa78f2adf8
Loann Peurey <loannpeurey@gmail.com>
2025-08-11 15:44:28 +0200

    feat: add a configModel class that lists and loads configuration from a toml file

---
+++ src/config/config.py
@@ -0,0 +1,46 @@
+import tomllib  # Python 3.11+
+from pathlib import Path
+from typing import Optional
+from dataclasses import dataclass
+
+from pydantic import BaseModel, HttpUrl, Field, ValidationError
+
+@dataclass
+class DatabaseConfig(BaseModel):
+    url: str = Field(min_length=1)
+
+@dataclass
+class JobsConfig(BaseModel):
+    handler: str = Field(min_length=1)
+    partition: Optional[str] = None
+
+@dataclass
+class HTTPConfig(BaseModel):
+    base_url: HttpUrl
+
+@dataclass
+class ConfigModel(BaseModel):
+    database: DatabaseConfig
+    jobs: JobsConfig
+    http: HTTPConfig
+
+
+def load_config(file_path: Path) -> ConfigModel:
+    """
+    loads the config for the service from a toml file
+
+    Args:
+        file_path: path of the toml config file
+
+    Returns:
+
+    """
+    with open(file_path, "rb") as f:
+        raw = tomllib.load(f)
+
+    try:
+        return ConfigModel.model_validate(raw)
+    except ValidationError as e:
+        print("Invalid configuration:")
+        print(e)
+        raise
