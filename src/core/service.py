58640bbb2ea98aac7ec8106cb0124067b2ea168d
Loann Peurey <loannpeurey@gmail.com>
2025-08-11 15:43:19 +0200

    feat: first draft of a service class able to interact with tools and runs the main loop

---
+++ src/core/service.py
@@ -0,0 +1,16 @@
+class Service:
+    """
+    Main service object, runs the main loop of the program, stores configuration and interaction objects
+    """
+
+    def __init__(self, db_uow, http_client, config):
+        self.db_uow = db_uow
+        self.http_client = http_client
+        self.config = config
+
+    def _one_loop(self):
+        raise NotImplementedError
+
+    def main_loop(self):
+        while True:
+            self._one_loop()
