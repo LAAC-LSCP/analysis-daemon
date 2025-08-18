f4330c2830333683441d2ab6facbc04a3b2c1ffe
Lawrence Borst <laurens.s.borst@gmail.com>
2025-08-18 15:05:21 +0200

    chore: delete the old stuff
    delete the stuff that was added as boilerplate at the start

--- src/task/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["Task", "TaskFactory", "TaskManager"]
-
-from .task import Task
-from .task_factory import TaskFactory
-from .task_manager import TaskManager
--- src/types/ids.py
+++
@@ -1,4 +0,0 @@
-from typing import NewType
-
-TaskId = NewType("TaskId", str)
-UserId = NewType("UserId", str)
