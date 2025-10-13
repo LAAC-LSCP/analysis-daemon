f5230475425c3d67a495b642f819ce75870d4a5b
Lawrence Borst <laurens.s.borst@gmail.com>
2025-10-13 16:03:39 +0200

    chore: move /tests/unit/core to /tests/unit/service_layer
    Test tree should follow the src tree
    In fact we remove the /tests/unit/core folder

--- tests/unit/service_layer/test_service.py
+++ tests/unit/service_layer/test_service.py
@@ -9,8 +9,8 @@ from src.domain import commands
 from src.service_layer.service import Service
 from src.service_layer.unit_of_work.publishing_uow import PublishingUoW
 from tests.integration.service_layer.fakes import FakeUoW
-from tests.unit.core.fake_handlers import FakeHandlers
-from tests.unit.core.fake_http_client import FakeHTTPClient
+from tests.unit.service_layer.fake_handlers import FakeHandlers
+from tests.unit.service_layer.fake_http_client import FakeHTTPClient
 
 
 @pytest.mark.asyncio
