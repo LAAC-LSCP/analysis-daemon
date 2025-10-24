3c21ff7e3bd2560bba068b752f55026b3c11048c
Lawrence Borst <laurens.s.borst@gmail.com>
2025-10-24 23:53:47 +0200

    test(conftest): simplify config fixture
    remove unnecessary fixtures in unit test conftest.py
    use the existing configuration.toml across all tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,16 @@
 import shutil
+import tempfile
+import tomllib
 from pathlib import Path
 from typing import Generator
 
 import pytest
+import tomli_w
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import Session, clear_mappers, sessionmaker
 
 from src.adapters.orm import metadata, start_mappers
+from src.config.config import ConfigModel, load_config
 
 
 @pytest.fixture
@@ -38,3 +42,48 @@ def session_factory(in_memory_db) -> Generator[sessionmaker]:
 @pytest.fixture
 def session(session_factory) -> Generator[Session]:
     return session_factory()
+
+
+@pytest.fixture(scope="session")
+def config_path() -> Generator[Path]:
+    current_dir: Path = Path(__file__).parent
+
+    config_file: Path = current_dir / "fake_filesystem" / "configuration.toml"
+
+    # The testing configuration.toml has relative paths. Need to replace with absolute
+    with tempfile.NamedTemporaryFile(delete=True) as tf:
+        config_as_toml: dict
+
+        with open(config_file, "rb") as f:
+            config_as_toml = tomllib.load(f)
+
+        config_as_toml = _replace_relative_with_absolute_paths(
+            config_as_toml,
+            config_file,
+        )
+
+        tomli_w.dump(config_as_toml, tf)
+        tf.flush()
+
+        yield Path(tf.name)
+
+
+def _replace_relative_with_absolute_paths(
+    config_as_toml: dict, config_file: Path
+) -> dict:
+    config_as_toml["filesystems"] = [
+        _fs_with_abs_path(fs, config_file) for fs in config_as_toml["filesystems"]
+    ]
+
+    return config_as_toml
+
+
+def _fs_with_abs_path(fs: dict, config_file: Path) -> dict:
+    fs["path"] = str((config_file.parent / fs["path"]).resolve())
+
+    return fs
+
+
+@pytest.fixture(scope="session")
+def config_model(config_path: Path) -> Generator[ConfigModel]:
+    yield load_config(config_path)
